ee294c0678b178bcd3c45393b2187570b686f9be
Project-Group2 <91367462+Project-Group2@users.noreply.github.com>
2021-09-29 12:27:27 +0530

    Delete Banking_Project_Backend directory

--- Banking_Project_Backend/Banking_Project/src/main/java/com/lti/demo/dao/BeneficiaryDao.java
+++
@@ -1,9 +0,0 @@
-package com.lti.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lti.demo.beans.Beneficiary;
-
-public interface BeneficiaryDao extends JpaRepository<Beneficiary, Long> {
-
-}

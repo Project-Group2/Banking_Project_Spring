0d11e170728f7e826510c32ce15d35ba54fe6914
Project-Group2 <91367462+Project-Group2@users.noreply.github.com>
2021-09-29 12:34:38 +0530

    Delete Banking_Project directory

--- Banking_Project/src/main/java/com/lti/demo/dao/CreateNetBankingAccountJpaDao.java
+++
@@ -1,9 +0,0 @@
-package com.lti.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lti.demo.beans.NetBankingAccount;
-
-public interface CreateNetBankingAccountJpaDao extends JpaRepository<NetBankingAccount, Long>{
-
-}
